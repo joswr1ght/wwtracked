1bba55b303f8af616491d50220cd2cca758d65be
Joshua Wright <jwright@hasborg.com>
2022-12-31 07:53:12 -0500

    Allow JWT to be specified with or without Bearer prefix; make quotes consistent

--- wwtracked.py
+++ wwtracked.py
@@ -45,28 +45,59 @@ def printfood(foods):
         try:
             foodportion = food['portionName']
             foodsize = food['portionSize']
-            suffix = f", {foodsize} {foodportion}"
+            suffix = f', {foodsize} {foodportion}'
         except KeyError:
             # Quick add foods lack a portion size and portion name
             suffix = ''
-        print(f"* {foodname}{suffix}")
+        print(f'* {foodname}{suffix}')
+
+
+"""
+Validate JWT. Return False if not valid.
+"""
+def checkjwt(jwt):
+    assert type(jwt) == str, 'JWT must be type str'
+    if jwt[0:3] != 'eyJ' and jwt[0:10] != 'Bearer eyJ':
+        return False
+    # With the Bearer prefix, the JWT I'm seeing is 1095 characters. Check for length
+    # at least 1000 to give us a sanity check of JWT
+    if len(jwt) < 1000:
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
     if (len(sys.argv) != 4):
-        sys.stderr.write(f"Usage: {sys.argv[0]} startdate enddate JWT\n")
+        sys.stderr.write(f'Usage: {sys.argv[0]} startdate enddate JWT\n')
         sys.stderr.write('\nDates must be in the format YYYY-MM-DD. Omit leading zeros.\n')
         sys.exit(1)
 
     startdate = datetime.date(*map(int, sys.argv[1].split('-')))
     enddate = datetime.date(*map(int, sys.argv[2].split('-')))
-    authheader = {'Authorization': f"Bearer {sys.argv[3]}"}
 
-    print(f"# Weight Watchers Tracked Food Report\n\n> {sys.argv[1]} - {sys.argv[2]}\n")
+    # Allow JWT to include 'Bearer ' prefix
+    if (sys.argv[3][0:7] == 'Bearer '):
+        jwt = sys.argv[3][7:]
+    else:
+        jwt = sys.argv[3]
+
+    if (checkjwt(jwt) == False):
+        sys.stderr.write('ERROR: Invalid JWT. Get a valid JWT from your logged-in Firefox session.\n')
+        sys.exit(-1)
+
+    authheader = {'Authorization': f'Bearer {jwt}'}
+
+    print(f'# Weight Watchers Tracked Food Report\n\n> {sys.argv[1]} - {sys.argv[2]}\n')
 
     for date in daterange(startdate, enddate):
         url = f'{ENDPOINT}/{date}'
         response = requests.get(url, headers=authheader)
+        if (response.status_code != 200):
+            sys.stderr.write(f'ERROR: Invalid response from weightwatchers.com API ({response.status_code}). ')
+            sys.stderr.write('Make sure you have a valid JWT from a logged-in browser session.\n')
+            exit(-1)
+
         trackedday = json.loads(response.content)
         print(f'\n\n## {date}')
 
